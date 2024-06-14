41c218fee1378417ec64ebbdc0fec889183ecae1
mccampero <mccampero2@gmail.com>
2024-06-14 09:59:39 -0400

    Bridgerton Quiz Game Final Push

--- js/questions.js
+++ js/questions.js
@@ -18,5 +18,40 @@ const questions = [
         question: "Where is Prince Friedrich from?",
         choices: ["Germany", "France", "Prussia", "Spain"],
         correct: "Prussia"
+    },
+    {
+        question: "Who does Anthony Bridgerton marry?",
+        choices: ["Kate","Sienna", "Edwina", "Cressida"],
+        correct: "Kate"
+    },
+    {
+        question: "What disorder does Lady Danbury help Simon overcome?",
+        choices: ["Stuttering", "Anxiety", "Insomnia", "ADHD"],
+        correct: "Stuttering"
+    },
+    {
+        question: "How many Bridgerton siblings are there?",
+        choices: ["5", "9", "6", "8"],
+        correct: "8"
+    },
+    {
+        question: "Where do Daphne and Simon first meet?",
+        choices: ["At a ball", "In the park", "At a sweet shop", "At Lady Danbury's house"],
+        correct: "At a ball"
+    },
+    {
+        question: "Where is the Bridgerton's family home located?",
+        choices: ["Grosvenor Square", "Regent Street", "Park Lane", "Mayfair"],
+        correct: "Grosvenor Square"
+    },
+    {
+        question: "Which Bridgerton sibling is an artist?",
+        choices: ["Anthony", "Daphne", "Colin", "Benedict"],
+        correct: "Benedict"
+    },
+    {
+        question: "Who does Colin Bridgerton almost marry?",
+        choices: ["Siena Rosso", "Penelope Featherington", "Marina Thompson", "Cressida Cowper"],
+        correct: "Marina Thompson"
     }
 ];
